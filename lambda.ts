6549ecb36945725ab73b3903b83b4833ec3abc7d
Thomas Schersach <tschersach@octo.com>
2018-07-19 09:47:36 +0200

    Added one comment on lambda imports

--- lambda.ts
+++ lambda.ts
@@ -1,5 +1,6 @@
 export const listEvents = async () => {
     // I would prefer being able to import outside of the lambda scope but this is not currently possible
+    // This is not even working anymore :(
     const {createResponse} = await import('./bin/utils')
     const {getAllBofEvents} = await import('./bin/dynamo')
 
